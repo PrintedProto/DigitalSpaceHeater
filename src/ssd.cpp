a5b477a6a57ee65e3ade42fa9417b6726f777d87
Joseffus Santos <joseffus.santos@gmail.com>
2017-01-07 21:55:10 -0600

    SoftSPI v0.02

--- src/ssd.cpp
+++ src/ssd.cpp
@@ -21,20 +21,20 @@ All text above, and the splash screen must be included in any redistribution
 #include <Adafruit_SSD1306.h>
 
 // If using software SPI (the default case):
-/*
+
 #define OLED_MOSI   9
 #define OLED_CLK   10
 #define OLED_DC    11
 #define OLED_CS    12
 #define OLED_RESET 13
 Adafruit_SSD1306 display(OLED_MOSI, OLED_CLK, OLED_DC, OLED_RESET, OLED_CS);
-*/
+/*
 // Uncomment this block to use hardware SPI
 #define OLED_DC     6
 #define OLED_CS     7
 #define OLED_RESET  8
 Adafruit_SSD1306 display(OLED_DC, OLED_RESET, OLED_CS);
-
+*/
 
 #define NUMFLAKES 10
 #define XPOS 0
