3d9c53df4a665a125ac763de2dfd0b3781ef94af
Joseffus Santos <joseffus.santos@gmail.com>
2017-01-08 20:08:31 -0600

    dht broken rot enc and oled functional

--- src/main.cpp
+++ src/main.cpp
@@ -33,6 +33,15 @@ volatile int oldEncPos = 20; //stores the last encoder position value so we can 
 volatile byte reading = 0; //somewhere to store the direct values we read from our interrupt pins before checking to see if we have moved a whole detent
 static int encButton = 4;
 
+//DHT22 sensor
+#define DHTPIN            6
+#define DHTTYPE           DHT22
+DHT_Unified dht(DHTPIN, DHTTYPE);
+uint32_t delayMS;
+
+//screen variables
+volatile int dhttemp, fintemp, foutemp, butpres, dhtrh, settemp;
+
 //encoder functions
 void PinA(){
   cli(); //stop interrupts happening before we read pin values
@@ -82,12 +91,44 @@ void setup()   {
   //attachInterrupt(digitalPinToInterrupt(pinA),PinA,RISING); // set an interrupt on PinA, looking for a rising edge signal and executing the "PinA" Interrupt Service Routine (below)
   //attachInterrupt(digitalPinToInterrupt(pinB),PinB,RISING); // set an interrupt on PinB, looking for a rising edge signal and executing the "PinB" Interrupt Service Routine (below)
   pinMode(13, OUTPUT);
+
+  //dht22 sensor
+  dht.begin(); //initializes dht22
+  // Print temperature sensor details.
+  settemp = 20;
+  dhttemp = 1;
+  dhtrh = 1;
+/*
+  // Print temperature sensor details.
+  sensor_t sensor;
+  dht.temperature().getSensor(&sensor);
+  Serial.println("------------------------------------");
+  Serial.println("Temperature");
+  Serial.print  ("Sensor:       "); Serial.println(sensor.name);
+  Serial.print  ("Driver Ver:   "); Serial.println(sensor.version);
+  Serial.print  ("Unique ID:    "); Serial.println(sensor.sensor_id);
+  Serial.print  ("Max Value:    "); Serial.print(sensor.max_value); Serial.println(" *C");
+  Serial.print  ("Min Value:    "); Serial.print(sensor.min_value); Serial.println(" *C");
+  Serial.print  ("Resolution:   "); Serial.print(sensor.resolution); Serial.println(" *C");
+  Serial.println("------------------------------------");
+  // Print humidity sensor details.
+  dht.humidity().getSensor(&sensor);
+  Serial.println("------------------------------------");
+  Serial.println("Humidity");
+  Serial.print  ("Sensor:       "); Serial.println(sensor.name);
+  Serial.print  ("Driver Ver:   "); Serial.println(sensor.version);
+  Serial.print  ("Unique ID:    "); Serial.println(sensor.sensor_id);
+  Serial.print  ("Max Value:    "); Serial.print(sensor.max_value); Serial.println("%");
+  Serial.print  ("Min Value:    "); Serial.print(sensor.min_value); Serial.println("%");
+  Serial.print  ("Resolution:   "); Serial.print(sensor.resolution); Serial.println("%");
+  Serial.println("------------------------------------");
+  delayMS = sensor.min_delay / 1000;
+  */
 }
 
-
-
-void updatescrn(uint8_t dhttemp, uint8_t settemp, uint8_t fintemp, uint8_t foutemp){
-
+//updates the screen
+//void updatescrn(uint8_t dhttemp, uint8_t dhtrh, uint8_t settemp, uint8_t fintemp, uint8_t foutemp){
+void updatescrn(){
   display.clearDisplay();
   display.setTextSize(1);
   display.setTextColor(WHITE);
@@ -99,53 +140,80 @@ void updatescrn(uint8_t dhttemp, uint8_t settemp, uint8_t fintemp, uint8_t foute
   display.setTextSize(1);
   display.println("");
   display.println("Fan.I " + String(fintemp) + "C    Coil ON");
-  display.println("Fan.O " + String(foutemp) + "C    hh:mm:ss");
+  display.println("Fan.O " + String(foutemp) + "C      " + String(dhtrh) + "%RH");
   display.display();
 }
 
+//adjusts Temp setpoint
+void adjustT(){
+  //Serial.println("IF statement triggered");
+  attachInterrupt(digitalPinToInterrupt(pinA),PinA,RISING); // set an interrupt on PinA and B, looking for a rising edge signal
+  attachInterrupt(digitalPinToInterrupt(pinB),PinB,RISING); // and executing the PinA() Interrupt Service Routine
+  //Serial.println("Interrupts attached");
+  while (butpres==0){
+    //Serial.println("While loop triggered");
+    if(oldEncPos != encoderPos) {
+      if(encoderPos <= 0){
+        encoderPos = 0;
+      }
+      else if(encoderPos >= 80){
+        encoderPos = 80;
+      }
+      oldEncPos = encoderPos;
+      //Serial.println("EncoderPos" + String(encoderPos));
+      }
+      updatescrn();
+      //Serial.println("update screen triggered");
+      if(!digitalRead(encButton)){
+        delay(1000);
+        butpres = !digitalRead(encButton);
+      }
+    }
+    detachInterrupt(digitalPinToInterrupt(pinA)); // Removes interrupts so values will only increment when needed
+    detachInterrupt(digitalPinToInterrupt(pinB));
+  //  Serial.println("Interrupts dettached");
+}
+
+void getDhtinfo(){
+  Serial.println("1");
+  sensors_event_t event;
+  Serial.println("2");
+  dht.temperature().getEvent(&event);
+  Serial.println("3");
+  if (isnan(event.temperature)) {
+    dhttemp = 0;
+    Serial.println(event.temperature);
+  }
+  else {
+    dhttemp = event.temperature;
+  }
+  // Get humidity event and print its value.
+  dht.humidity().getEvent(&event);
+  if (isnan(event.relative_humidity)) {
+    dhtrh = 0;
+  }
+  else {
+    dhtrh = event.relative_humidity;
+  }
+  delay(delayMS);
+}
+
 void loop() {
-  //.println("Loop start");
-  uint8_t dhttemp,fintemp, foutemp, butpres; // settemp,
-  dhttemp = 88;
-  //settemp = 20;
+  //Serial.println("Loop");
   fintemp = 90;
   foutemp = 91;
+  //Serial.println("Polling DHT");
+  //getDhtinfo();
+  Serial.println("Temp= " + String(dhttemp));
+  Serial.println("Humi= " + String(dhtrh));
   butpres = digitalRead(encButton);
   //digitalWrite(13, !butpres);
   //Serial.println("butpres =" + String(butpres));
   if (butpres==0){
-    //Serial.println("IF statement triggered");
-    attachInterrupt(digitalPinToInterrupt(pinA),PinA,RISING); // set an interrupt on PinA and B, looking for a rising edge signal
-    attachInterrupt(digitalPinToInterrupt(pinB),PinB,RISING); // and executing the PinA() Interrupt Service Routine
-    Serial.println("Interrupts attached");
-    while (butpres==0){
-      //Serial.println("While loop triggered");
-      if(oldEncPos != encoderPos) {
-        if(encoderPos <= 0){
-          encoderPos = 0;
-        }
-        else if(encoderPos >= 80){
-          encoderPos = 80;
-        }
-        oldEncPos = encoderPos;
-        Serial.println("EncoderPos" + String(encoderPos));
-        }
-        updatescrn(dhttemp, encoderPos, fintemp, foutemp);
-        //Serial.println("update screen triggered");
-        if(!digitalRead(encButton)){
-          delay(1000);
-          butpres = !digitalRead(encButton);
-        }
-        //digitalWrite(13, !butpres);
-        //Serial.println("butpres =" + String(butpres));
+    adjustT();
       }
-    detachInterrupt(digitalPinToInterrupt(pinA)); // Removes interrupts so values will only increment when needed
-    detachInterrupt(digitalPinToInterrupt(pinB));
-    Serial.println("Interrupts dettached");
-    delay(60000);
-    }
 
-  updatescrn(dhttemp, encoderPos, fintemp, foutemp);
-
+  updatescrn();
+  Serial.println("Scrn updated");
 
 }
